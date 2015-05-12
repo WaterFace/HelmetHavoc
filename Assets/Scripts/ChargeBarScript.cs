db1680246259782805813ef465e3b01542c25da6
Kevin Visser-Weckworth <KevinVisserWeckworth@gmail.com>
2015-05-12 17:08:23 -0400

    Added charge bar to show jetpack charge amount

---
+++ Assets/Scripts/ChargeBarScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChargeBarScript : MonoBehaviour {
+
+    public Transform full;
+
+    public void SetAngle(float angle)
+    {
+        float currentAngle = transform.rotation.eulerAngles.z;
+        transform.Rotate(new Vector3(0f, 0f, angle - currentAngle));
+    }
+
+    public void SetValue(float value)
+    {
+        full.localScale = new Vector3(value,
+                                      full.localScale.y,
+                                      full.localScale.z);
+    }
+}
