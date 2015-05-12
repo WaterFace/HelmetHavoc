10361b9de4a6dab44d6259bdf681a4dabc2499a3
Kevin Visser-Weckworth <KevinVisserWeckworth@gmail.com>
2015-05-12 17:08:58 -0400

    Added basic jetpack functionality, including charging and directional jumping

---
+++ Assets/Scripts/PlayerJetpackScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerJetpackScript : MonoBehaviour {
+
+    private Vector2 mouseWorldPos { get { return Camera.main.ScreenToWorldPoint(Input.mousePosition); } }
+    private Vector2 direction { get { return ((Vector3)mouseWorldPos - transform.position).normalized; } }
+    private bool ignoreGround = false;
+    private bool grounded;
+    private Rigidbody2D rb;
+    private int blockingMask;
+    private float charge;
+    private bool checkGround;
+    
+    public Transform groundCheck;
+    public ChargeBarScript chargeBar;
+    public float minFlySpeed;
+    public float maxFlySpeed;
+    public float chargeSpeed;
+    
+    [HideInInspector] public bool flying;
+
+	void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        blockingMask = 1 << LayerMask.NameToLayer("Ground");
+	}
+	
+	void Update()
+    {
+        grounded = Physics2D.Linecast(transform.position, groundCheck.position, blockingMask) && checkGround;
+
+        if (Input.GetButton("Fire1"))
+        {
+            charge += chargeSpeed * Time.deltaTime;
+        }
+
+        charge = Mathf.Clamp(charge, 0.0f, 1.0f);
+        chargeBar.SetAngle(Mathf.Rad2Deg*Mathf.Atan2(direction.y, direction.x));
+        chargeBar.SetValue(charge);
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            rb.AddForce(direction * Mathf.Lerp(minFlySpeed, maxFlySpeed, charge), ForceMode2D.Impulse);
+            charge = 0f;
+        }
+
+        chargeBar.gameObject.SetActive(!(charge <= 0f));
+	}
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(transform.position, ((Vector3)mouseWorldPos - transform.position).normalized * 3f);
+    }
+}
