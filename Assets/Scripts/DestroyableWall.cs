90c5ac865e59efe15992f653ddb6834ac6e66b48
Kevin Visser-Weckworth <kevinvisserweckworth@gmail.com>
2015-06-04 21:34:52 -0400

    the rest of the everything

--- Assets/Scripts/DestroyableWall.cs
+++ Assets/Scripts/DestroyableWall.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class DestroyableWall : MonoBehaviour {
 
     public GameObject particle;
+    public GameObject[] extraParticles;
+
 
     void HitByPlayer()
     {
@@ -12,5 +14,13 @@ public class DestroyableWall : MonoBehaviour {
 
         Destroy(ps, ps.GetComponent<ParticleSystem>().duration);
         Destroy(this.gameObject);
+        
+        foreach (var part in extraParticles)
+        {
+            var eps = Instantiate<GameObject>(part);
+            eps.transform.position = this.transform.position;
+
+            Destroy(eps, eps.GetComponent<ParticleSystem>().duration);
+        }
     }
 }
